b50d50d7fabfb453e405009596c65bfb6eb10891
Nícolas Wojcichoski <nicolas.dsw@gmail.com>
2020-08-19 19:40:11 -0300

    feat: form modes

--- src/app/pages/posts/form/form.component.ts
+++ src/app/pages/posts/form/form.component.ts
@@ -1,6 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Post } from '../model/post';
 import { PostsService } from '../services/posts.service';
 
@@ -10,17 +13,69 @@ import { PostsService } from '../services/posts.service';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  public id: number;
+  public mode: 'add' | 'edit';
   public post$: Observable<Post>;
+  public formGroup = new FormGroup({
+    title: new FormControl(''),
+    body: new FormControl(''),
+  });
+  private object: Post;
+  private emptyObject: Post = { title: '', body: '', id: null, userId: null };
 
-  constructor(private route: ActivatedRoute, private postService: PostsService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location,
+    private postService: PostsService,
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(({ id }) => {
-      this.id = +id;
-      if (Number.isInteger(this.id)) {
-        this.post$ = this.postService.findById(this.id);
+      let loadSub: Observable<Post> = null;
+      if (id === 'add') {
+        this.mode = 'add';
+        loadSub = of({ ...this.emptyObject });
+      } else if (Number.isInteger(+id)) {
+        this.mode = 'edit';
+        loadSub = this.postService.findById(+id);
+      } else {
+        this.goBack();
       }
+      this.post$ = loadSub.pipe(
+        map((post) => {
+          if (post) {
+            this.object = post;
+            this.formGroup.setValue({
+              title: post.title,
+              body: post.body,
+            });
+          }
+          return post;
+        }),
+      );
     });
+  }
+
+  submit() {
+    const save = {
+      add: (object: Post) => {
+        this.postService.insert(object).subscribe((id) => {
+          alert(`${id} Cadastrado com sucesso`);
+          this.router.navigate(['./', id]);
+        });
+      },
+      edit: (object: Post) => {
+        this.postService.update(object.id, object).subscribe(() => {
+          alert('Salvo com sucesso');
+        });
+      },
+    };
+    const { title, body } = this.formGroup.value;
+    const objectToSend = { ...this.object, title, body };
+    save[this.mode](objectToSend);
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
--- src/app/pages/posts/services/posts.service.ts
+++ src/app/pages/posts/services/posts.service.ts
@@ -1,11 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { Post } from '../model/post';
 import { SwrService } from 'src/app/shared/spring-http/services/swr.service';
 import { PageRequest } from 'src/app/shared/spring-http/model/page-request';
 import { PageResponse } from 'src/app/shared/spring-http/model/page-response';
 import { SearchUtil } from 'src/app/shared/spring-http/utils/search-util';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -22,5 +23,32 @@ export class PostsService {
 
   findById(id: number): Observable<Post> {
     return this.swrService.get(`${this.serviceUrl}/${id}`);
+  }
+
+  insert(object: Post) {
+    return this.http
+      .post<number>(this.serviceUrl, object)
+      .pipe(catchError((e: HttpErrorResponse) => this.handleError(e)));
+  }
+
+  update(id: number, object: Post) {
+    return this.http
+      .put<void>(`${this.serviceUrl}/${id}`, object)
+      .pipe(catchError((e: HttpErrorResponse) => this.handleError(e)));
+  }
+
+  handleError({ error, ...e }: HttpErrorResponse) {
+    if (error.status === 400 && error.errors) {
+      let messages = '';
+      error.errors.forEach(({ field, defaultMessage }) => {
+        messages += `${field}: ${defaultMessage}\n`;
+      });
+      console.log(messages);
+      alert(messages);
+    } else {
+      console.log(e.message);
+      alert(JSON.stringify(error.message));
+    }
+    return throwError(e);
   }
 }
